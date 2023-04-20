5d44662073d899dbfb20f5f68f31276cd0b59d27
dmatidza <dmatidza@gmail.com>
2023-04-20 16:19:53 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include <stdio.hi>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry 
+ * Return: Always
  */
 int main(void)
 {
 
-        printf("with proper grammar, but the outcome is a piece of art.\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entr
+ * Return: Always
  */
 int main(void)
 {
-        printf("Size of a char: % byte(s)\n", sizeof(char));
-        printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %d byte(s)\n", sizeof(float));
+        printf("Size of a char: %i byte(s)\n", sizeof(char));
+        printf("Size of a long int: %i byte(s)\n", sizeo(int));
+        printf("Size of a long long int: %i byte(s)\n", sizeof(long int));
+        printf("Size of a float: %i byte(s)\n", sizeof(float));
         return (0);
 }
